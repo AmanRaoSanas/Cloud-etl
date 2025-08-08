f3d884a59ac89b9e689727a902c96d563ffb7951
Aman Rao Sanas <amanraosanas@Amans-MacBook-Air.local>
2025-08-08 16:33:54 +0530

    basic ETL Job

---
+++ etl_jobs/etl_job.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import datetime
+import os
+
+# Absolute project path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # This gets the project root (cloud-ETL)
+
+# Input and Output paths (absolute)
+input_path = os.path.join(project_root, 'data/raw/sales_data.csv')
+output_path = os.path.join(project_root, 'data/processed/cleaned_sales_data.csv')
+
+os.makedirs(os.path.dirname(output_path), exist_ok= True)
+
+df = pd.read_csv(input_path)
+
+df = df.dropna()
+
+# Changing the date format to dd/mm/yyyy
+df["Date"] = pd.to_datetime(df["Date"])
+df["Date"] = df["Date"].dt.strftime("%d/%m/%Y")
+
+# Sorting data based on date
+sorted_df = df.sort_values(by= "Date", ascending= True)
+
+sorted_df["Sale_ID"] = [f"S{i:03}" for i in range (1, len(sorted_df)+1)]
+
+sorted_df.to_csv(output_path, index=False)
+
+print(f"ETL Completed. Cleaned data written to: {output_path}")
